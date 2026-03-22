b6e85e990250397dab1f10881698f6ba0687718a
ROHIN <michaelrohin@gmail.com>
2026-03-22 11:57:18 +0530

    time changed

--- src/components/CountdownClock/CountdownClock.tsx
+++ src/components/CountdownClock/CountdownClock.tsx
@@ -4,8 +4,8 @@ const CountdownClock = () => {
     const [timeLeft, setTimeLeft] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
 
     useEffect(() => {
-        // Set target date to March 25, 2026 at 11:59:59 AM
-        const targetDate = new Date('2026-03-25T11:59:59');
+        // Set target date to March 25, 2026 at 11:59:59 PM
+        const targetDate = new Date('2026-03-25T23:59:59');
 
         const interval = setInterval(() => {
             const now = new Date();
